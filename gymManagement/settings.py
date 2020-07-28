a8438440957b526ae8fd0722360b0e7ff24b3bbe
chencho gyeltshen <chenchogyeltshen000@gmail.com>
2020-07-28 12:09:42 +0530

    debug

--- gymManagement/settings.py
+++ gymManagement/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#$i&(x8ze*eir%u_3klgu&^=brci7mljtdn0-yef0@9@9!^q03'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
